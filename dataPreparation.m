bf659c848beaa8952718b3247a1f4174cfac9d7b
Kian <seyed.jalaleddini@mail.mcgill.ca>
2016-09-30 10:14:23 -0700

    data preperation concatinating all data files

---
+++ dataPreparation.m
@@ -0,0 +1,61 @@
+numFiles = 3;
+fileName = cell(numFiles,1);
+fileName{1} = 'dataTemp/test1.txt';
+fileName{2} = 'dataTemp/test1.txt';
+fileName{3} = 'dataTemp/test1.txt';
+
+extensorLength = [];
+extensorMeasuredForce = [];
+extensorDesiredForce = [];
+extensorGammaStatic = [];
+extensorGammaDynamic = [];
+extensorCorticalDrive = [];
+
+flexorLength = [];
+flexorMeasuredForce = [];
+flexorDesiredForce = [];
+flexorGammaStatic = [];
+flexorGammaDynamic = [];
+flexorCorticalDrive = [];
+
+newTrial = [];
+time = [];
+%Check column number from the header
+%%
+for fileIndex = 1 : numFiles
+    disp('Concatinating dataTemp 1')
+    dataTemp = dlmread(fileName{fileIndex},',',1,0);
+    time = [time;dataTemp(:,1)];
+    extensorLength = [extensorLength; dataTemp(:,2)];
+    extensorDesiredForce = [extensorDesiredForce; dataTemp(:,11)];
+    extensorMeasuredForce = [extensorMeasuredForce; dataTemp(:,4)];
+    extensorGammaStatic = [extensorGammaStatic; dataTemp(:,13)];
+    extensorGammaDynamic = [extensorGammaDynamic; dataTemp(:,14)];
+    extensorCorticalDrive = [extensorCorticalDrive;dataTemp(:,17)];
+
+    flexorLength = [flexorLength; dataTemp(:,3)];
+    flexorDesiredForce = [flexorDesiredForce; dataTemp(:,12)];
+    flexorMeasuredForce = [flexorMeasuredForce; dataTemp(:,5)];
+    flexorGammaStatic = [flexorGammaStatic; dataTemp(:,15)];
+    flexorGammaDynamic = [flexorGammaDynamic; dataTemp(:,16)];
+    flexorCorticalDrive = [flexorCorticalDrive;dataTemp(:,18)];
+    newTrial = [newTrial;dataTemp(:,19)];
+end
+
+data.time = time;
+data.extensorLength = extensorLength;
+data.extensorDesiredForce = extensorDesiredForce;
+data.extensorMeasuredForce = extensorMeasuredForce;
+data.extensorGammaStatic = extensorGammaStatic;
+data.extensorGammaDynamic = extensorGammaDynamic;
+data.extensorCorticalDrive = extensorCorticalDrive;
+
+data.flexorLength = flexorLength;
+data.flexorDesiredForce = flexorDesiredForce;
+data.flexorMeasuredForce = flexorMeasuredForce;
+data.flexorGammaStatic = flexorGammaStatic;
+data.flexorGammaDynamic = flexorGammaDynamic;
+data.flexorCorticalDrive = flexorCorticalDrive;
+data.newTrial = newTrial;
+data.time = time;
+save dataAllFiles data
