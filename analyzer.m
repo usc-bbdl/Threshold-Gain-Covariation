890b12f2bca7ba7b438179cd8abfedadcf28f040
Kian <seyed.jalaleddini@mail.mcgill.ca>
2016-10-18 15:55:42 -0700

    Adding muscle 2 for analysis

--- analyzer.m
+++ analyzer.m
@@ -1,36 +1,43 @@
 % The goal of this project is to see how position threshold and velocity threshold change as a function of gamma (both dynamic and static)
-% 
-
-
 clear all
 clc
-load data/test1
+load data/test6
 %%
 perturbationAmp = 10; %p-p amplitude of perturbations
 holdTime = 3 / 2;%hold time is trial length/2
-%gammaRange = [0 100 200]'; protocol_reader.m will take care of it
-%velocityRange = [10 40 70 100]'; protocol_reader.m will take care of it
-%positionRange = [75,25,-25,-75]'; protocol_reader.m will take care of it
-[ gammaRange,positionRange,velocityRange,number_of_trials ] = protocol_reader( data(:,2) )
-
+gammaDynamicRange = [0 200]';
+gammaStaticRange = [0]';
+velocityRange = [20 100]';
+positionRange = [-30,0,30]';
+muscleChoice = 2;
 expProtChan = 2;
-forceChannel = 7;
-lengthChannel = 3;
+if muscleChoice == 1
+    lengthChannel = 3;
+    forceChannel = 7;
+elseif muscleChoice == 2
+    lengthChannel = 4;
+    forceChannel = 8;
+else
+    error('Muscle choice must be either 1 or 2');
+end
 displayFlag = 0;
-reflexAmplitude = zeros(length(gammaRange),length(gammaRange),...
+reflexAmplitude = zeros(length(gammaDynamicRange),length(gammaStaticRange),...
     length(positionRange),length(velocityRange));
+muscleLength = zeros(length(gammaDynamicRange),length(gammaStaticRange),...
+    length(positionRange),length(velocityRange));
+
 totalSteps = length(reflexAmplitude(:));
 step = 1;
 h = waitbar(0,'Processing');
-for gammaDynamicIndex = 1 : length(gammaRange)                              % Gamma range for dynamic - 1st dim
-    for gammaStaticIndex = 1 : length(gammaRange)                           % Gamma range for static - 2nd dim
+for gammaDynamicIndex = 1 : length(gammaDynamicRange)                              % Gamma range for dynamic - 1st dim
+    for gammaStaticIndex = 1 : length(gammaStaticRange)                           % Gamma range for static - 2nd dim
         for positionIndex = 1 : length(positionRange)                       % position range - 3nd dim
             for velocityIndex = 1 : length(velocityRange)                   % velocity range - 4th dim
                 waitbar(step/totalSteps);
                 step = step + 1;
                 
-                expCondition = [gammaRange(gammaDynamicIndex),gammaRange(gammaStaticIndex),...
-                    0,gammaRange(gammaDynamicIndex),gammaRange(gammaStaticIndex),0,...
+                expCondition = [gammaDynamicRange(gammaDynamicIndex),gammaStaticRange(gammaStaticIndex),...
+                    0,gammaDynamicRange(gammaDynamicIndex),gammaStaticRange(gammaStaticIndex),0,...
                     positionRange(positionIndex),velocityRange(velocityIndex)];
                 indexFound = experimentalConditionFinder(data(:,expProtChan),expCondition);
                 %plot(data(indexFound(1):indexFound(2),7))
@@ -41,12 +48,21 @@ for gammaDynamicIndex = 1 : length(gammaRange)                              % Ga
                     lengthThisTrial = data(indexFound(1):indexFound(2),lengthChannel);
                     forceThisTrial = data(indexFound(1):indexFound(2),forceChannel);
                     expProtThisTrial = data(indexFound(1):indexFound(2),expProtChan);
-                    reflexAmplitudeThisTrial = reflexAmplitudeCalculator(expProtThisTrial,...
-                        forceThisTrial,perturbationAmp,velocityRange(velocityIndex),holdTime);
+                    dataThisTrial.Length = lengthThisTrial;
+                    dataThisTrial.Force = forceThisTrial;
+                    dataThisTrial.expProtocol = expProtThisTrial;
+                    perturbationParams.pertAmp = perturbationAmp;
+                    perturbationParams.pertVel = velocityRange(velocityIndex);
+                    perturbationParams.holdTime = holdTime;
+                    [reflexAmplitudeThisTrial,muscleLengthThisTrial] =...
+                        reflexAmplitudeCalculator(dataThisTrial,perturbationParams,muscleChoice);
+                    
                     reflexAmplitude(gammaDynamicIndex,gammaStaticIndex,positionIndex...
                         ,velocityIndex) = reflexAmplitudeThisTrial;
+                    muscleLength(gammaDynamicIndex,gammaStaticIndex,positionIndex...
+                        ,velocityIndex) = muscleLengthThisTrial;
                     if displayFlag == 1
-                        disp(['Gd: ',num2str(gammaRange(gammaDynamicIndex)),', Gs: ',num2str(gammaRange(gammaStaticIndex))...
+                        disp(['Gd: ',num2str(gammaDynamicRange(gammaDynamicIndex)),', Gs: ',num2str(gammaStaticRange(gammaStaticIndex))...
                             ', Pos: ',num2str(positionRange(positionIndex)),', vel: ',num2str(velocityRange(velocityIndex))])
                         disp(['Reflex amplitude was: ',num2str(reflexAmplitudeThisTrial)])
                     end
@@ -58,7 +74,11 @@ for gammaDynamicIndex = 1 : length(gammaRange)                              % Ga
 end
 close(h)
 %% Visualization
-display('everything is saved in the file named as reflexAmplitude')
-
-%results_visualization(reflexAmplitude, 'gamma_d',1,'vel',1,positionRange,velocityRange) % please use help results_visualization for full details
+%display('everything is saved in the file named as reflexAmplitude')
+close all
+figure
+subplot(2,1,1)
+results_visualization(muscleLength,'gamma_d',1,'pos',2,positionRange,velocityRange) % please use help results_visualization for full details
+subplot(2,1,2)
+results_visualization(reflexAmplitude,'gamma_d',1,'pos',2,positionRange,velocityRange) % please use help results_visualization for full details
 % results_visualization is a function to visualize the results
--- reflexAmplitudeCalculator.m
+++ reflexAmplitudeCalculator.m
@@ -1,16 +1,38 @@
-function reflexAmplitude = reflexAmplitudeCalculator(expProt,force,perturbationAmplitude,perturbationVelocity,holdTime)
+function [reflexAmplitude,muscleLengthAmplitude] = reflexAmplitudeCalculator(data,perturbParams,muscleChoice)
+    if nargin == 2
+        muscleChoice = 1;
+    end
+    if muscleChoice == 1
+        trialOnsetFlag = -1;
+    elseif muscleChoice == 2
+        trialOnsetFlag = -2;
+    else
+        error('Muscle choice must be either 1 or 2');
+    end
     plotFlag = 0;
+    
+    expProt = data.expProtocol;
+    mLength = data.Length;
+    force = data.Force;
+    
+    perturbationAmplitude = perturbParams.pertAmp;
+    perturbationVelocity = perturbParams.pertVel;
+    holdTime = perturbParams.holdTime;
+%%    
     samplingFreq = 1000;
     perturbationDuration = floor(samplingFreq * (perturbationAmplitude / perturbationVelocity));
     holdTime = floor(samplingFreq * holdTime);
-    perturbationOnetIndex = find(expProt == -1);
+    perturbationOnetIndex = find(expProt == trialOnsetFlag);
     perturbationOnetIndex = perturbationOnetIndex(2:end);
     xAxis = 1: length(force);
     reflexAmplitudeTemp = zeros(length(perturbationOnetIndex),1);
+    muscleLengthAmplitudeTemp = zeros(length(perturbationOnetIndex),1);
     for i = 1 : length(perturbationOnetIndex)
         forcePhasic = force(perturbationOnetIndex(i):perturbationOnetIndex(i)+perturbationDuration+holdTime);
         forceTonic = force(perturbationOnetIndex(i)+perturbationDuration+holdTime-200:perturbationOnetIndex(i)+perturbationDuration+holdTime-100);
-        reflexAmplitudeTemp = max(forcePhasic) - mean(forceTonic);
+        lengthAmp = mLength(perturbationOnetIndex(i):perturbationOnetIndex(i)+perturbationDuration+holdTime);
+        reflexAmplitudeTemp(i) = max(forcePhasic) - mean(forceTonic);
+        muscleLengthAmplitudeTemp(i) = mean(lengthAmp);
     end
     if plotFlag == 1
         figure(100)
@@ -25,4 +47,5 @@ function reflexAmplitude = reflexAmplitudeCalculator(expProt,force,perturbationA
         close(100)
     end
     reflexAmplitude = mean(reflexAmplitudeTemp);
+    muscleLengthAmplitude = mean(muscleLengthAmplitudeTemp);
 end
