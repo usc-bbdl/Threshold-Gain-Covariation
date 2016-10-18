f5613b49ac6efd0e415ab7ec8e521d6a95210d6b
Ali Marjaninejad <Ali Marjaninejad>
2016-10-17 20:48:34 -0700

    visualization function is added

--- dataPreparation.m
+++ dataPreparation.m
@@ -1,2 +1,2 @@
-data = dlmread('data/realTimeData2016_10_05_17_15_50.txt',',',3,0); % reads the txt file
+data = dlmread('data/realTimeData2016_10_14_17_22_02.txt',',',3,0); % reads the txt file
 save data/test1 data                                                % saves the MATLAB file
