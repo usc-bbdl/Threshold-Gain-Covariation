f8d03b67fcd3fa2aeda2b02214a92071710c3018
maral kasiri <maral kasiri>
2017-02-19 18:00:19 -0800

    Data Concatination Completed

--- Data_Concatination.m
+++ Data_Concatination.m
@@ -1,5 +1,7 @@
+
+AllData=[];
 for i =1:25
-    data = dlmread(['data/finalProtocol',num2str(i),'Data.txt'],',',3,0); % reads the txt file
-    save (['data/sweep_',num2str(i)], 'data')                                                % saves the MATLAB file
-    
+    data = dlmread(['dataProtocolRev2_',num2str(i),'.txt'],',',2,0); % reads the txt file
+   
+   AllData=[AllData;data(:,1:8)];
 end
